315d7d211ed06a0c24713516f5d4d832937a3154
YourLocalCommunist1924 <lightweightgunsmith@gmail.com>
2026-05-14 15:23:47 -0600

    test

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 console.log("Hello world");
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <h3>THIS IS A REACT TEST!</h3>
+  </React.StrictMode>);
